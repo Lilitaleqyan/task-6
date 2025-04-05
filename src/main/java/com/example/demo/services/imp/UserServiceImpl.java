2c9ebf2fe4b7f6cf4e77cbe16fd8451475da9d5f
Lilitaleqyan <150114568+Lilitaleqyan@users.noreply.github.com>
2025-04-05 20:18:55 +0400

    task6

--- src/main/java/com/example/demo/services/imp/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/imp/UserServiceImpl.java
@@ -41,4 +41,9 @@ public class UserServiceImpl implements UserService {
         }
         return false;
     }
+
+    @Override
+    public User getByUser(User user) {
+        return userDao.getByUser(user);
+    }
 }
